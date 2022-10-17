74dd488ff3152515a8e8f5ac95f3d2354af4776a
Enzo <enzoc432@gmail.com>
2022-10-17 14:06:23 -0300

    main

---
+++ JavaScript/testejs.js
@@ -0,0 +1,7 @@
+window.alert("Boa tarde galera!")
+function exibir_janela_alerta()
+{
+    var name = window.prompt ("Por favor digite seu nome aqui primeiro", "<Seu nome aqui>");
+
+    window.alert("Olá" + name + "! Tudo brother?")
+}
