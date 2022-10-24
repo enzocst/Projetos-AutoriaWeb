77939b5a01beab75959dfa1c433a649eaa26bda4
Enzo <enzoc432@gmail.com>
2022-10-24 14:14:17 -0300

    main

--- JavaScript/testejs.js
+++ JavaScript/testejs.js
@@ -12,16 +12,64 @@ function exibir_janela_alerta()
 function exibir_num_objbody()
 {
     var name = window.prompt ("Por favor digite seu nome aqui primeiro", "<Seu nome aqui>");
-    if (name != null && name != "")
+    if (name != null && name != "");
     {
     divexibirnome.innerHTML = string("Olá" + name + "! Tudo bem?");
     }
 }
-function somar()
-{
-    var numero1 = parseInt(window.prompt ("Digite um numero"));
-    var numero2 = parseInt(window.prompt ("Digite outro numero"));
+function somar(){
+    var numero1 = parseInt(window.prompt ("Digite um número."));
+    while (isNaN(numero1)){
+        window.alert("Ops, vc não digitou um número inteiro!")
+        var numero1 = parseInt(window.prompt ("Digite um número."));
+    }
+    var numero2 = parseInt(window.prompt ("Digite outro número."));
+    while (isNaN(numero2)){
+        window.alert("Ops, vc não digitou um número inteiro!")
+        var numero2 = parseInt(window.prompt ("Digite outro número."));
+    }
     var resultado = numero1 + numero2;
-    divexibirnome.innerHTML = string(resultado);
-
+    divexibirresultado.innerHTML = String(resultado);
+}
+function subtrair(){
+    var numero1 = parseInt(window.prompt ("Digite um número."));
+    while (isNaN(numero1)){
+        window.alert("Ops, vc não digitou um número inteiro!")
+        var numero1 = parseInt(window.prompt ("Digite um número."));
+    }
+    var numero2 = parseInt(window.prompt ("Digite outro número."));
+    while (isNaN(numero2)){
+        window.alert("Ops, vc não digitou um número inteiro!")
+        var numero2 = parseInt(window.prompt ("Digite outro número."));
+    }
+    var resultado = numero1 - numero2;
+    divexibirresultadosub.innerHTML = String(resultado);
+}
+function multiplicar(){
+    var numero1 = parseInt(window.prompt ("Digite um número."));
+    while (isNaN(numero1)){
+        window.alert("Ops, vc não digitou um número inteiro!")
+        var numero1 = parseInt(window.prompt ("Digite um número."));
+    }
+    var numero2 = parseInt(window.prompt ("Digite outro número."));
+    while (isNaN(numero2)){
+        window.alert("Ops, vc não digitou um número inteiro!")
+        var numero2 = parseInt(window.prompt ("Digite outro número."));
+    }
+    var resultado = numero1 * numero2;
+    divexibirresultadomulti.innerHTML = String(resultado);
+}
+function dividir(){
+    var numero1 = parseInt(window.prompt ("Digite um número."));
+    while (isNaN(numero1)){
+        window.alert("Ops, vc não digitou um número inteiro!")
+        var numero1 = parseInt(window.prompt ("Digite um número."));
+    }
+    var numero2 = parseInt(window.prompt ("Digite outro número."));
+    while (isNaN(numero2)){
+        window.alert("Ops, vc não digitou um número inteiro!")
+        var numero2 = parseInt(window.prompt ("Digite outro número."));
+    }
+    var resultado = numero1 / numero2;
+    divexibirresultadodiv.innerHTML = String(resultado);
 }
