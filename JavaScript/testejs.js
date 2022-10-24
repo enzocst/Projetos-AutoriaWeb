e311b213b8bddf8d2e54599af521f036053e0d9b
Enzo <enzoc432@gmail.com>
2022-10-24 13:45:15 -0300

    main

--- JavaScript/testejs.js
+++ JavaScript/testejs.js
@@ -1,15 +1,27 @@
-window.alert("Boa tarde galera!")
+//  window.alert("Boa tarde galera!")
 function exibir_janela_alerta()
 {
     var name = window.prompt ("Por favor digite seu nome aqui primeiro", "<Seu nome aqui>");
     if (name !=null && name !="")
     {
-    window.alert("Olá" + name + "! Tudo brother?");
+//    window.alert("Olá" + name + "! Tudo brother?");
     } else {
-        window.alert("Digita o texto animal!!")
+//        window.alert("Digita o texto animal!!")
     }
 }
 function exibir_num_objbody()
 {
-    var name = window.prompt ("Digite seu nome")
+    var name = window.prompt ("Por favor digite seu nome aqui primeiro", "<Seu nome aqui>");
+    if (name != null && name != "")
+    {
+    divexibirnome.innerHTML = string("Olá" + name + "! Tudo bem?");
+    }
+}
+function somar()
+{
+    var numero1 = parseInt(window.prompt ("Digite um numero"));
+    var numero2 = parseInt(window.prompt ("Digite outro numero"));
+    var resultado = numero1 + numero2;
+    divexibirnome.innerHTML = string(resultado);
+
 }
